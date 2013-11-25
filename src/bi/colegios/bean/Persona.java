2cb45ec3dda33a4ac450e17eebd29ed0b9908726
jorge.vilca <jorge.vilca@essalud.gob.pe>
2013-11-25 08:19:00 -0500

    test

--- src/bi/colegios/bean/Persona.java
+++ src/bi/colegios/bean/Persona.java
@@ -19,9 +19,6 @@ import org.hibernate.annotations.LazyCollectionOption;
 @Entity
 @Table(name="persona")
 public class Persona implements Serializable {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue
